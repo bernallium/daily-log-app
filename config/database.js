28e2b948fcf91f5191404700f3e6a3ce517584f9
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-02 22:58:44 -0400

    Add config/database.js

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true // Options to avoid deprecation warnings
+});
+
+const db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
