5838aff510f20bb123da4e330b6baa0dcc840c99
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-03 15:17:57 -0400

    Add Login Page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import {Route, NavLink} from 'react-router-dom';
+import LoginPage from '../LoginPage/LoginPage';
 
 function App() {
-  return (
+  return (  
     <div className="App">
       <header className="App-header">
         <nav>
-          <NavLink exact to='/'>LIST</NavLink>
+          <NavLink exact to='/'>Home</NavLink>
           &nbsp;&nbsp;&nbsp;
-          <NavLink exact to='/add'>ADD</NavLink>
+          <NavLink to='/add'>Add</NavLink>
+          &nbsp;&nbsp;&nbsp;
+          <NavLink to='/login'>Log In</NavLink>
         </nav>
       </header>
+      <Route path='/login' component={LoginPage}/>
     </div>
   );
 }
