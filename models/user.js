8c93b2b50a7a57a1dd32f232004ebb58590bbe58
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-03 22:08:32 -0400

    Add password to User model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: {type: String, required: true},
+  email: {type: String, required: true, lowercase: true, unique: true},
   first_name: {type: String, required: true},
   last_name: {type: String, required: true},
+  password: String
 },{
   timestamps: true
+});
+
+// Seriliazes user document (object) into JSON while first removing the password
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    // remove the password property when serializing doc to JSON
+    delete ret.password;
+    return ret;
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
