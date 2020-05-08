d51a3b2bff9c994d2b5e63a60174d0ffab6d2608
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-07 21:16:40 -0400

    Update Daily header

--- src/components/Daily/Daily.js
+++ src/components/Daily/Daily.js
@@ -13,7 +13,17 @@ const Daily = () => {
 
   return (
     <div className='daily'>
-      <h2>{dd}/{mm} {dayOfWeek} <span role='img' aria-label='plant'>🌱</span> </h2>
+      <div>
+        <div className='daily-header'>
+          <h2 className='dd'>17</h2>
+          <div className='day-month-container'>
+            <h3 className='day'>Thurs</h3>
+            <h3 className='month'>May</h3>
+          </div>
+        </div>
+        <hr/>
+        {/* <h2>{dd}/{mm} {dayOfWeek} <span role='img' aria-label='plant'>🌱</span> </h2> */}
+      </div>
       <p contentEditable='true'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Sagittis nisl rhoncus mattis rhoncus urna neque. Nisl purus in mollis nunc. Quam pellentesque nec nam aliquam sem et tortor consequat. Hendrerit gravida rutrum quisque non tellus orci. Platea dictumst quisque sagittis purus sit amet volutpat consequat. Cras sed felis eget velit aliquet sagittis id consectetur. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Tortor dignissim convallis aenean et. Praesent elementum facilisis leo vel fringilla est. Viverra mauris in aliquam sem. In dictum non consectetur a. Nunc sed augue lacus viverra vitae congue eu. Aenean sed adipiscing diam donec. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Laoreet suspendisse interdum consectetur libero id faucibus. Ipsum faucibus vitae aliquet nec ullamcorper. Neque egestas congue quisque egestas. Et ultrices neque ornare aenean euismod elementum nisi quis.</p>
       <div className='add-content'>+ Add content</div>
     </div>
