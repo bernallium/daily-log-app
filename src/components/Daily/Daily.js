2586246e57bcffd326a4e4eb773433db04034f99
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-08 01:09:02 -0400

    Add Date functions to get string representations of days as YYYYMMDD

--- src/components/Daily/Daily.js
+++ src/components/Daily/Daily.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Daily.css';
+import Day from '../Day/Day';
 
 const Daily = () => {
 
@@ -12,23 +13,43 @@ const Daily = () => {
   const mm = String(today.getMonth() + 1); //January is 0!
   let dayOfWeek = DAYS_OF_WEEK[today.getDay()];
   let month = MONTHS[today.getMonth()];
+  const currentWeek = getCurrentWeek();
   
+  // Returns the string representation of a Date object as 'YYYYMMDD'
+  function formatDate(dateObj) {
+    let month = '' + (dateObj.getMonth() + 1);
+    let day = '' + dateObj.getDate();
+    const year = dateObj.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join(''); // Pass any character in join as as separator
+  }
+
+  // Retrurns an array of strings representing days of the current week (Mon-Fri) as 'YYYYMMDD'
+  function getCurrentWeek() {
+    const curr = new Date();
+    const first = curr.getDate() - curr.getDay();
+    const week = [];
+  
+    for (let i = 1; i < 7; i++) {
+      let next = new Date(curr.getTime());
+      next.setDate(first + i);
+      next = formatDate(next);
+      week.push(next);
+    }
+    return week;
+  }
 
   return (
     <div className='daily'>
-      <div>
-        <div className='daily-header'>
-          <h2 className='dd'>{dd}</h2>
-          <div className='day-month-container'>
-            <h3 className='day'>{dayOfWeek}</h3>
-            <h3 className='month'>{month}</h3>
-          </div>
-        </div>
-        <hr/>
-        {/* <h2>{dd}/{mm} {dayOfWeek} <span role='img' aria-label='plant'>🌱</span> </h2> */}
-      </div>
-      <p contentEditable='true'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Sagittis nisl rhoncus mattis rhoncus urna neque. Nisl purus in mollis nunc. Quam pellentesque nec nam aliquam sem et tortor consequat. Hendrerit gravida rutrum quisque non tellus orci. Platea dictumst quisque sagittis purus sit amet volutpat consequat. Cras sed felis eget velit aliquet sagittis id consectetur. Accumsan tortor posuere ac ut consequat semper viverra nam libero. Tortor dignissim convallis aenean et. Praesent elementum facilisis leo vel fringilla est. Viverra mauris in aliquam sem. In dictum non consectetur a. Nunc sed augue lacus viverra vitae congue eu. Aenean sed adipiscing diam donec. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Laoreet suspendisse interdum consectetur libero id faucibus. Ipsum faucibus vitae aliquet nec ullamcorper. Neque egestas congue quisque egestas. Et ultrices neque ornare aenean euismod elementum nisi quis.</p>
-      <div className='add-content'>+ Add content</div>
+      {currentWeek.map(day => 
+        <Day 
+          day={day} 
+        />
+        )
+      }
     </div>
   );
 }
